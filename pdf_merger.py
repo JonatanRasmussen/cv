40f8dd16f5dfa9a5671ba71a326d12e4bd09f447
JonatanRasmussen <115923496+JonatanRasmussen@users.noreply.github.com>
2026-07-27 16:05:03 +0200

    Omskrivning af CV

--- pdf_merger.py
+++ pdf_merger.py
@@ -2,16 +2,16 @@ from pypdf import PdfReader, PdfWriter
 import os
 
 base_path = r"C:\Users\BudoB\OneDrive\Dokumenter Tekst\Personlig\Joblog"
-pdf_1_path = os.path.join(base_path, "jonatan_cv.pdf")
-pdf_2_path = os.path.join(base_path, "jonatan_ansoegning.pdf")
-output_path = os.path.join(base_path, "jonatan_cv_and_ansoegning.pdf")
+pdf_1_path = os.path.join(base_path, "MSc_diploma_and_grades_JonatanRasmussen.pdf")
+pdf_2_path = os.path.join(base_path, "BSc_diploma_and_grades_JonatanRasmussen.pdf")
+output_path = os.path.join(base_path, "MSc+BSc_MERGED_diploma_and_grades_JonatanRasmussen.pdf")
 
 writer: PdfWriter = PdfWriter()
 pdf_1_reader: PdfReader = PdfReader(stream=pdf_1_path)
 for page in pdf_1_reader.pages:
     writer.add_page(page)
 pdf_2_reader: PdfReader = PdfReader(stream=pdf_2_path)
-for page in pdf_2_reader.pages:
+for page in pdf_2_reader.pages:  # To only include the first 3 pages: pdf_2_reader.pages[:3]:
     writer.add_page(page)
 with open(file=output_path, mode="wb") as f:
     writer.write(stream=f)
